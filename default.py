054b695873f50fc77072f4c7a56924546981e933
uvjim <git@simpsoncomputers.co.uk>
2016-09-24 22:06:10 +0100

    Allow setting of a timer that expires after the currently playing item

--- default.py
+++ default.py
@@ -12,6 +12,19 @@ import resources.lib.logger as logger
 def promptTimeout():
     return xbmcgui.Dialog().input(language(32072), type=xbmcgui.INPUT_NUMERIC)
 
+def getCurrentItemTimeout():
+    ret = None
+    timeout = xbmc.getInfoLabel('Player.TimeRemaining')
+    if timeout:
+        logger.write("getCurrentTimeout: {}".format(timeout))
+        timeout = timeout.split(":")
+        if len(timeout) == 2:
+            ret = int(timeout[0]) + 1
+        elif len(timeout) == 3:
+            ret = (int(timeout[0]) * 60) + int(timeout[1]) + 1
+        logger.write("getCurrentTimeout: {}".format(ret))
+    return ret
+
 ####################################################################
 #    Main processing
 ####################################################################
@@ -27,12 +40,20 @@ if __name__ == "__main__":
         xbmcaddon.Addon().openSettings()
     else:
         language = xbmcaddon.Addon().getLocalizedString
+        settings = xbmcaddon.Addon().getSetting
         alarm = Alarm(name='uvjim.rpiturnoff', friendly=language(32070))
         if xbmc.Player().isPlaying(): # only do something if the Player is playing something
             if action == 'queryset':
                 if not alarm.isSet(): # no timer is set so let's create one
-                    timeout = promptTimeout()
-                    if timeout: alarm.set(timeout=timeout)
+                    timertype = 0
+                    if settings('general.timertype') == 'true': # let's workout the timer type
+                        types = [language(32081), language(32082)]
+                        timertype = xbmcgui.Dialog().select(language(32080), types)
+                    if timertype == 0:
+                        timeout = promptTimeout()
+                    elif timertype == 1:
+                        timeout = getCurrentItemTimeout()
+                    if timertype >= 0 and timeout: alarm.set(timeout=timeout)
                 else: # there is a timer so let's get details for it
                     aRemaining = alarm.getTimeLeft()
                     if aRemaining > 60:
@@ -44,7 +65,7 @@ if __name__ == "__main__":
                     ans = xbmcgui.Dialog().yesno(language(32070), language(32071).format('{} {}'.format(aRemaining, aUnits)), nolabel=language(32998), yeslabel=language(32999).format(alarm.friendly))
                     if ans: # need to take an action
                         logger.write('Showing edit choices')
-                        actions = [language(32077), language(32078)]
+                        actions = [language(32077), language(32078), language(32082)]
                         sel = xbmcgui.Dialog().select(language(32999).format(alarm.friendly), actions)
                         logger.write('Edit action: {}'.format(sel))
                         if sel == 0: # cancel timer
@@ -52,5 +73,10 @@ if __name__ == "__main__":
                         elif sel == 1: # extend timer
                             extendby = promptTimeout()
                             if extendby: alarm.extend(extendby=extendby)
+                        elif sel == 2: # after current item
+                            timeout = getCurrentItemTimeout()
+                            if timeout:
+                                alarm.cancel(notify=False)
+                                alarm.set(timeout=timeout)
             elif action == 'expired': # expire the timer
                 alarm.expired()
